8d24b11458a9e37ad3d86552f012a32a0199ff48
Thomas Burguiere <thomas.burguiere@gmail.com>
2016-08-25 06:39:52 +0200

    first meaningful in container test

--- src/test/java/com/causy/rest/resources/EmployeeResourceAppTest.java
+++ src/test/java/com/causy/rest/resources/EmployeeResourceAppTest.java
@@ -1,6 +1,8 @@
 package com.causy.rest.resources;
 
+import com.causy.model.Employee;
 import com.causy.persistence.dao.BasicDAO;
+import org.assertj.core.api.Assertions;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -11,13 +13,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.inject.Inject;
+import javax.ws.rs.core.Response;
 import java.io.File;
+import java.net.URISyntaxException;
 
 @RunWith(Arquillian.class)
 public class EmployeeResourceAppTest {
 
     @Inject
-    BasicDAO basicDAO;
+    EmployeeResource resource;
 
     @Deployment
     public static WebArchive createDeployment() {
@@ -39,13 +43,17 @@ public class EmployeeResourceAppTest {
         WebArchive causyPocWar = ShrinkWrap.createFromZipFile(WebArchive.class, new File("./target/tomcat-jersey-weld-hibernate.war"));
         causyPocWar.addAsLibraries(lib);
 
-        System.out.println(causyPocWar.toString(true));
+        // replacing prod hibernate settings in war with test properties
+        causyPocWar.addAsResource(new File("src/test/resources/hibernate.properties"));
 
         return causyPocWar;
     }
 
     @Test
-    public void test() {
-        System.out.println(basicDAO);
+    public void test() throws Exception {
+        final Response employeeCreationResponse = resource.createEmployee(new Employee("Thomas", "admin"));
+
+
+        Assertions.assertThat(employeeCreationResponse.getStatus()).isEqualTo(201);
     }
 }
